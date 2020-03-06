f1139a4b13718953e3306a9bf2493d5927bacdcf
María Paz Morales Llopis <mpmoralesll@gmail.com>
2020-03-05 22:31:52 -0300

    Revert "Revert "Modificaciónes para API""
    This reverts commit 37deed69fd52255bfb532de9da3271d363360240.

--- proyectoprogramacion/src/views/Contenido.js
+++ proyectoprogramacion/src/views/Contenido.js
@@ -116,66 +116,216 @@ class Parrafo extends Component{
 
 class Subseccion extends Component{
 	render(){
+		const contenidos = this.props.contenidos.map((x,index) => {
+			if (x.tipo == "parrafo"){
+				return(
+					<Parrafo key={index} parrafo={x.descripcion}/>
+				);
+			}
+			else if (x.tipo == "definicion"){
+				return (
+					<Definicion key={index} titulo={x.titulo} definicion={x.descripcion}/>
+				);
+			}
+			else if (x.tipo == "video"){
+				return(
+					<Video key={index} videolink={x.link}/>
+				);
+			}
+			else if (x.tipo == "ecuacion"){
+				return(
+					<Ecuacion key={index} contenido={x.contenido}/>
+				);
+			}
+			else if (x.tipo == "codigo"){
+				return(
+					<Codigo key={index} lenguaje={x.lenguaje} codigo={x.codigo}/>			
+				);
+			}
+			else if (x.tipo == "alerta"){
+				return(
+					<Alerta key={index} color={this.props.color} titulo={x.titulo}contenido={x.descripcion} />
+				);
+			}
+			else if (x.tipo == "frase"){
+				return(
+					<Frase key={index} autor={x.autor} frase={x.descripcion}/>
+				);
+			}
+			else if (x.tipo == "imagen"){
+				return(
+					<Imagen key={index} imagenLink={x.link} />
+				);
+			}
+        });
 		return(
 			<div id={"section"+this.props.numero} style={{paddingTop:"5px"}}>
 				<hr></hr>
 				<h2 style={{marginTop:"25px",marginBottom:"15px"}}>{this.props.titulo}</h2>
-				<Parrafo parrafo="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Vestibulum id metus ac nisl bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet sagittis. In tincidunt orci sit amet elementum vestibulum. Vivamus fermentum in arcu in aliquam. Quisque aliquam porta odio in fringilla. Vivamus nisl leo, blandit at bibendum eu, tristique eget risus. Integer aliquet quam ut elit suscipit, id interdum neque porttitor. Integer faucibus ligula."/>
-				<Definicion titulo="Terminal" definicion="Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content."/>
-				<Parrafo parrafo="Pulvinar leo id risus pellentesque vestibulum. Sed diam libero, sodales eget sapien vel, porttitor bibendum enim. Donec sed nibh vitae lorem porttitor blandit in nec ante. Pellentesque vitae metus ipsum. Phasellus sed nunc ac sem malesuada condimentum. Etiam in aliquam lectus. Nam vel sapien diam. Donec pharetra id arcu eget blandit. Proin imperdiet mattis augue in porttitor. Quisque tempus enim id lobortis feugiat. Suspendisse tincidunt risus quis dolor fringilla blandit. Ut sed sapien at purus lacinia porttitor. Nullam iaculis, felis a pretium ornare, dolor nisl semper tortor, vel sagittis lacus est consequat eros. Sed id pretium nisl. Curabitur dolor nisl, laoreet vitae aliquam id."/>
-				<Video videolink='https://www.youtube.com/watch?v=proaHoj2_-g'/>
-				<Parrafo parrafo="Quis quam ut magna consequat faucibus. Pellentesque eget nisi a mi suscipit tincidunt. Ut tempus dictum risus. Pellentesque viverra sagittis quam at mattis. Suspendisse potenti. Aliquam sit amet gravida nibh, facilisis gravida odio. Phasellus auctor velit at lacus blandit, commodo iaculis justo viverra. Etiam vitae est arcu. Mauris vel congue dolor. Aliquam eget mi mi. Fusce quam tortor, commodo ac dui quis, bibendum viverra erat. Maecenas mattis lectus enim, quis tincidunt dui molestie euismod. Curabitur et diam tristique, accumsan nunc eu, hendrerit tellus."/>
-				<Ecuacion contenido = "En esta línea va una bella ecuación que contiene un $x^2$"/> 
-				<Ecuacion contenido="$$\int_0^\infty x^2 dx$$"/>
-				<Parrafo parrafo="Orci facilisis, dignissim tortor vitae, ultrices mi. Vestibulum a iaculis lacus. Phasellus vitae convallis ligula, nec volutpat tellus. Vivamus scelerisque mollis nisl, nec vehicula elit egestas a. Sed luctus metus id mi gravida, faucibus convallis neque pretium. Maecenas quis sapien ut leo fringilla tempor vitae sit amet leo. Donec imperdiet tempus placerat. Pellentesque pulvinar ultrices nunc sed ultrices. Morbi vel mi pretium, fermentum lacus et, viverra tellus. Phasellus sodales libero nec dui convallis, sit amet fermentum sapien auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed eu elementum nibh, quis varius libero."/>
-				<Codigo lenguaje="java" codigo={'public class MyClass {\n  public static void main(String[] args) {\n    for (int i = 0; i < 10; i++) {\n      if (i == 4) {\n        continue;\n      }\n      System.out.println(i);\n    }  \n  }\n}'}/>			
-				<Alerta color={this.props.color} titulo="Importante" contenido="En esta sección va algo muy importante que explica algún error común o algo por el estilo" />
-				<Parrafo parrafo="Nam eget purus nec est consectetur vehicula. Nullam ultrices nisl risus, in viverra libero egestas sit amet. Etiam porttitor dolor non eros pulvinar malesuada. Vestibulum sit amet est mollis nulla tempus aliquet. Praesent luctus hendrerit arcu non laoreet. Morbi consequat placerat magna, ac ornare odio sagittis sed. Donec vitae ullamcorper purus. Vivamus non metus ac justo porta volutpat."/>
-				<Frase autor="Pablo Picasso" frase="Las computadoras son inútiles. Sólo pueden darte respuestas."/>
-				<Parrafo parrafo="Nam eget purus nec est consectetur vehicula. Nullam ultrices nisl risus, in viverra libero egestas sit amet. Etiam porttitor dolor non eros pulvinar malesuada. Vestibulum sit amet est mollis nulla tempus aliquet. Praesent luctus hendrerit arcu non laoreet. Morbi consequat placerat magna, ac ornare odio sagittis sed. Donec vitae ullamcorper purus. Vivamus non metus ac justo porta volutpat."/>
-				<Imagen imagenLink={imagen2} />
-				<Parrafo parrafo="Pulvinar leo id risus pellentesque vestibulum. Sed diam libero, sodales eget sapien vel, porttitor bibendum enim. Donec sed nibh vitae lorem porttitor blandit in nec ante. Pellentesque vitae metus ipsum. Phasellus sed nunc ac sem malesuada condimentum. Etiam in aliquam lectus. Nam vel sapien diam. Donec pharetra id arcu eget blandit. Proin imperdiet mattis augue in porttitor. Quisque tempus enim id lobortis feugiat. Suspendisse tincidunt risus quis dolor fringilla blandit. Ut sed sapien at purus lacinia porttitor. Nullam iaculis, felis a pretium ornare, dolor nisl semper tortor, vel sagittis lacus est consequat eros. Sed id pretium nisl. Curabitur dolor nisl, laoreet vitae aliquam id."/>
+				{contenidos}
 				<ul>{/*ol para numerada*/ }
 					<li>Lorem ipsum dolor sit amet</li>
 					<li>Consectetur adipiscing elit</li>
 					<li>Integer molestie lorem at massa</li>
 					<li>Facilisis in pretium nisl aliquet</li>
 				</ul>
-				<Parrafo parrafo="Pulvinar leo id risus pellentesque vestibulum. Sed diam libero, sodales eget sapien vel, porttitor bibendum enim. Donec sed nibh vitae lorem porttitor blandit in nec ante. Pellentesque vitae metus ipsum. Phasellus sed nunc ac sem malesuada condimentum. Etiam in aliquam lectus. Nam vel sapien diam. Donec pharetra id arcu eget blandit. Proin imperdiet mattis augue in porttitor. Quisque tempus enim id lobortis feugiat. Suspendisse tincidunt risus quis dolor fringilla blandit. Ut sed sapien at purus lacinia porttitor. Nullam iaculis, felis a pretium ornare, dolor nisl semper tortor, vel sagittis lacus est consequat eros. Sed id pretium nisl. Curabitur dolor nisl, laoreet vitae aliquam id."/>
 			</div>
 		)
 	}
 }
 
+const dataSeccion = {
+	titulo: "Título del Primer Contenido",
+	numero: 1,
+	lista_secciones: ["Sección 1", "Sección 2", "Sección 3", "Sección 4", "Sección 5"],
+	id_anterior: 11,
+	id_siguiente: 13,
+	nivel: "Básico",
+    color: "warning",
+    lista_contenidos:[
+		[
+			{
+				tipo: "parrafo",
+				descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Vestibulum id metus ac nisl bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet sagittis. In tincidunt orci sit amet elementum vestibulum. Vivamus fermentum in arcu in aliquam. Quisque aliquam porta odio in fringilla. Vivamus nisl leo, blandit at bibendum eu, tristique eget risus. Integer aliquet quam ut elit suscipit, id interdum neque porttitor. Integer faucibus ligula."
+			},
+			{
+				tipo: "definicion",
+				titulo: "Terminal",
+				descripcion: "Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content."
+			} ,
+			{
+				tipo: "parrafo",
+				descripcion: "Pulvinar leo id risus pellentesque vestibulum. Sed diam libero, sodales eget sapien vel, porttitor bibendum enim. Donec sed nibh vitae lorem porttitor blandit in nec ante. Pellentesque vitae metus ipsum. Phasellus sed nunc ac sem malesuada condimentum. Etiam in aliquam lectus. Nam vel sapien diam. Donec pharetra id arcu eget blandit. Proin imperdiet mattis augue in porttitor. Quisque tempus enim id lobortis feugiat. Suspendisse tincidunt risus quis dolor fringilla blandit. Ut sed sapien at purus lacinia porttitor. Nullam iaculis, felis a pretium ornare, dolor nisl semper tortor, vel sagittis lacus est consequat eros. Sed id pretium nisl. Curabitur dolor nisl, laoreet vitae aliquam id."
+			},
+			{
+				tipo: "video",
+				link: "'https://www.youtube.com/watch?v=proaHoj2_-g'"
+			} ,
+			{
+				tipo: "parrafo",
+				descripcion: "Quis quam ut magna consequat faucibus. Pellentesque eget nisi a mi suscipit tincidunt. Ut tempus dictum risus. Pellentesque viverra sagittis quam at mattis. Suspendisse potenti. Aliquam sit amet gravida nibh, facilisis gravida odio. Phasellus auctor velit at lacus blandit, commodo iaculis justo viverra. Etiam vitae est arcu. Mauris vel congue dolor. Aliquam eget mi mi. Fusce quam tortor, commodo ac dui quis, bibendum viverra erat. Maecenas mattis lectus enim, quis tincidunt dui molestie euismod. Curabitur et diam tristique, accumsan nunc eu, hendrerit tellus."
+			},
+			{
+				tipo: "ecuacion",
+				contenido: "En esta línea va una bella ecuación que contiene un $x^2$."
+			} ,
+			{
+				tipo: "ecuacion",
+				contenido: '$$\\int_0^ \\infty x^2 dx$$'
+			},
+			{
+				tipo: "parrafo",
+				descripcion: "Orci facilisis, dignissim tortor vitae, ultrices mi. Vestibulum a iaculis lacus. Phasellus vitae convallis ligula, nec volutpat tellus. Vivamus scelerisque mollis nisl, nec vehicula elit egestas a. Sed luctus metus id mi gravida, faucibus convallis neque pretium. Maecenas quis sapien ut leo fringilla tempor vitae sit amet leo. Donec imperdiet tempus placerat. Pellentesque pulvinar ultrices nunc sed ultrices. Morbi vel mi pretium, fermentum lacus et, viverra tellus. Phasellus sodales libero nec dui convallis, sit amet fermentum sapien auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed eu elementum nibh, quis varius libero."
+			},
+			{
+				tipo: "codigo",
+				lenguaje: "java",
+				codigo: "public class MyClass {\n  public static void main(String[] args) {\n    for (int i = 0; i < 10; i++) {\n      if (i == 4) {\n        continue;\n      }\n      System.out.println(i);\n    }  \n  }\n}"
+			},
+			{
+				tipo: "alerta",
+				titulo: "Importante",
+				descripcion: "En esta sección va algo muy importante que explica algún error común o algo por el estilo."
+			},
+			{
+				tipo:"parrafo",
+				descripcion:"Nam eget purus nec est consectetur vehicula. Nullam ultrices nisl risus, in viverra libero egestas sit amet. Etiam porttitor dolor non eros pulvinar malesuada. Vestibulum sit amet est mollis nulla tempus aliquet. Praesent luctus hendrerit arcu non laoreet. Morbi consequat placerat magna, ac ornare odio sagittis sed. Donec vitae ullamcorper purus. Vivamus non metus ac justo porta volutpat."
+			},
+			{
+				tipo: "frase",
+				autor: "Pablo Picasso",
+				descripcion: "Las computadoras son inútiles. Sólo pueden darte respuestas."
+			},
+			{
+				tipo:"parrafo",
+				descripcion:"Nam eget purus nec est consectetur vehicula. Nullam ultrices nisl risus, in viverra libero egestas sit amet. Etiam porttitor dolor non eros pulvinar malesuada. Vestibulum sit amet est mollis nulla tempus aliquet. Praesent luctus hendrerit arcu non laoreet. Morbi consequat placerat magna, ac ornare odio sagittis sed. Donec vitae ullamcorper purus. Vivamus non metus ac justo porta volutpat."
+			},
+			{
+				tipo: "imagen",
+				link: imagen2
+			},
+			{
+				tipo: "parrafo",
+				descripcion: "Pulvinar leo id risus pellentesque vestibulum. Sed diam libero, sodales eget sapien vel, porttitor bibendum enim. Donec sed nibh vitae lorem porttitor blandit in nec ante. Pellentesque vitae metus ipsum. Phasellus sed nunc ac sem malesuada condimentum. Etiam in aliquam lectus. Nam vel sapien diam. Donec pharetra id arcu eget blandit. Proin imperdiet mattis augue in porttitor. Quisque tempus enim id lobortis feugiat. Suspendisse tincidunt risus quis dolor fringilla blandit. Ut sed sapien at purus lacinia porttitor. Nullam iaculis, felis a pretium ornare, dolor nisl semper tortor, vel sagittis lacus est consequat eros. Sed id pretium nisl. Curabitur dolor nisl, laoreet vitae aliquam id."
+			},
+
+		],
+		[
+			{
+				tipo: "parrafo",
+				descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Vestibulum id metus ac nisl bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet sagittis. In tincidunt orci sit amet elementum vestibulum. Vivamus fermentum in arcu in aliquam. Quisque aliquam porta odio in fringilla. Vivamus nisl leo, blandit at bibendum eu, tristique eget risus. Integer aliquet quam ut elit suscipit, id interdum neque porttitor. Integer faucibus ligula."
+			},
+		],
+		[
+			{
+				tipo: "parrafo",
+				descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Vestibulum id metus ac nisl bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet sagittis. In tincidunt orci sit amet elementum vestibulum. Vivamus fermentum in arcu in aliquam. Quisque aliquam porta odio in fringilla. Vivamus nisl leo, blandit at bibendum eu, tristique eget risus. Integer aliquet quam ut elit suscipit, id interdum neque porttitor. Integer faucibus ligula."
+			},
+		],
+		[
+			{
+				tipo: "parrafo",
+				descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Vestibulum id metus ac nisl bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet sagittis. In tincidunt orci sit amet elementum vestibulum. Vivamus fermentum in arcu in aliquam. Quisque aliquam porta odio in fringilla. Vivamus nisl leo, blandit at bibendum eu, tristique eget risus. Integer aliquet quam ut elit suscipit, id interdum neque porttitor. Integer faucibus ligula."
+			},
+		],
+		[
+			{
+				tipo: "parrafo",
+				descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Vestibulum id metus ac nisl bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet sagittis. In tincidunt orci sit amet elementum vestibulum. Vivamus fermentum in arcu in aliquam. Quisque aliquam porta odio in fringilla. Vivamus nisl leo, blandit at bibendum eu, tristique eget risus. Integer aliquet quam ut elit suscipit, id interdum neque porttitor. Integer faucibus ligula."
+			},
+		],
+	]
+};
+
 export default class Contenido extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-            indice: ["1","2","3","4","5"]
-        }
-	}
     render(){
-		const { indice } = this.state;
 		//Recordar cambiar key por id unico. Parece ser un asunto de vida o muerte. 
-		const indiceCompleto = indice.map((x,index) => {
+		const indiceCompleto = dataSeccion.lista_secciones.map((x,index) => {
 			return (
-			<a key={index} className="list-group-item list-group-item-action" href={"#section"+x}>Section {x}</a>
+			<a key={index} className="list-group-item list-group-item-action" href={"#section"+(index+1)}>{x}</a>
 			)
 		});
-		const secciones = indice.map((x,index) => {
+		const secciones = dataSeccion.lista_secciones.map((x,index) => {
 			return(
-				<Subseccion key={index} numero={x} titulo={"Subsección "+x} color={this.props.color}/>
+				<Subseccion key={index} numero={index+1} titulo={x} color={dataSeccion.color} contenidos={(dataSeccion.lista_contenidos)[index]}/>
 			)
 		});
+		var flechas = 
+			<> 
+				<a href={"/contenido/"+dataSeccion.id_anterior} className="text-muted small float-left">
+					<i className="icono fas fa-chevron-left fa-sm"></i>sección anterior 
+				</a>
+				<a href={"/contenido/"+dataSeccion.id_siguiente} className="text-muted small float-right">
+					sección siguiente<i className="icono fas fa-chevron-right fa-sm"></i>
+				</a>
+			</>;
+		
+		if (dataSeccion.id_anterior == null){
+			flechas = 
+			<> 
+				<a href={"/contenido/"+dataSeccion.id_siguiente} className="text-muted small float-right">
+					sección siguiente<i className="icono fas fa-chevron-right fa-sm"></i>
+				</a>
+			</>;
+		}
+		else if (dataSeccion.id_siguiente == null){
+			flechas = 
+			<> 
+				<a href={"/contenido/"+dataSeccion.id_anterior} className="text-muted small float-left">
+					<i className="icono fas fa-chevron-left fa-sm"></i>sección anterior 
+				</a>
+			</>;
+		}
+		
      	var Latex = require('react-latex');
         return(
         	<section>
 				<ul className="nav nav-pills nav-fill mb-3 bg-light text-left">
                     <li className="nav-item niveles ">
-                        <div className={"nav-link rounded-0 text-white active clearfix has-text-center align-items-center bg-"+this.props.color}>
+                        <div className={"nav-link rounded-0 text-white active clearfix has-text-center align-items-center bg-"+ dataSeccion.color}>
                             <div className="container">
-								<div className="float-left"> <i className="fas fa-terminal icono"></i>{this.props.nivel}</div>
-								<a style={{paddingTop:"15px"}} className="float-right text-white h6 font-weight-light " href={"/estudiantes/"+this.props.nivel}>
+								<div className="float-left"> <i className="fas fa-terminal icono"></i>{dataSeccion.nivel}</div>
+								<a style={{paddingTop:"15px"}} className="float-right text-white h6 font-weight-light " href={"/estudiantes/"+dataSeccion.nivel}>
 									<i className="icono fas fa-chevron-left fa-sm"></i>volver a secciones
 								</a>
 							</div>
@@ -188,17 +338,12 @@ export default class Contenido extends Component {
 							<div className="list-group sticky-list">
 								{indiceCompleto}
 								<div className="clearfix" style={{marginTop:"10px",marginBottom:"25px"}}>
-									<a href="\estudiantes" className="text-muted small float-left">
-									<i className="icono fas fa-chevron-left fa-sm"></i>sección anterior 
-									</a>
-									<a href="\estudiantes" className="text-muted small float-right">
-										sección siguiente<i className="icono fas fa-chevron-right fa-sm"></i>
-									</a>
+									{flechas}
 								</div>
 							</div>
 						</div>
 						<div className="col-sm-9" >
-							<h1 className="sicky-list display-4">{this.props.numero}. {this.props.titulo}</h1>
+							<h1 className="sicky-list display-4">{dataSeccion.numero}. {dataSeccion.titulo}</h1>
 							{secciones}
 						</div>
 					</div>
