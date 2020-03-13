7c7b64cf655adb70fa23ae9f858e3b9ada23cd6a
María Paz Morales Llopis <mpmoralesll@gmail.com>
2020-03-13 16:54:02 -0300

    Actualización Formulario

--- proyectoprogramacion/src/views/Inscripcion.js
+++ proyectoprogramacion/src/views/Inscripcion.js
@@ -1,16 +1,106 @@
 import React, { Component } from 'react'
 import './Inscripcion.css';
 
+const lista_sedes= {
+    basico: [
+        {
+            sede: "Universidad 1",
+            link: null
+        },
+        {
+            sede: "Universidad 2",
+            link: null
+        },
+        {
+            sede: "Universidad 3",
+            link: null
+        }
+    ],
+    intermedio: [
+        {
+            sede: "Universidad 11",
+            link: null
+        },
+        {
+            sede: "Universidad 22",
+            link: null
+        },
+        {
+            sede: "Universidad 33",
+            link: null
+        }
+    ],
+    avanzado: [
+        {
+            sede: "Universidad 111",
+            link: null
+        },
+        {
+            sede: "Universidad 222",
+            link: null
+        },
+        {
+            sede: "Universidad 333",
+            link: null
+        }
+    ]
+
+}
+
 export default class Inscripcion extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            nivel:null, 
+            sede:null
+        }
+    }
     render(){
         return(
             <div className="container" style={{paddingTop:"40px",paddingBottom:"40px"}}>
                 <form>
-                    <div className="row">
+                <div className="row">
                         <div className="col-md-2"></div>
                         <div className="col-md-8">
                             <h1 className="display-4" style={{fontSize:"2.5rem"}}>Formulario de Inscripción Talleres de Programación</h1>
                             <br></br>
+                            <h5>Datos del Taller</h5>
+                            <hr></hr>
+                            <p>Selecciona el nivel del taller de programación (básico, medio o avanzado), y luego escoge en cuál de las sedes disponibles te quieres inscribir. </p>
+                        </div>
+                        <div className="col-md-2"></div>
+                    </div>
+                    <div className="form-row">
+                        <div className="col-md-2"></div>
+                        <div className="form-group col-md-4">
+                            <label htmlFor="nivel">Nivel</label>
+                            <select id="nivel" className="form-control" onChange={(val) => this.setState({nivel:val.target.value})}>
+                                <option defaultValue>Básico</option>
+                                <option>Intermedio</option>
+                                <option>Avanzado</option>
+                            </select>
+                            {"hola"+this.state.nivel }
+                            { console.log(this.state.nivel)}
+                        </div>
+                        <div className="form-group col-md-4">
+                            <label htmlFor="sede">Sede</label>
+                            <select id="sede" className="form-control">
+                                <option defaultValue>Selecciona...</option>
+                                {lista_sedes[basico].map((x,index) => {
+                                    return (
+                                    <option key={index}>{x.sede}</option>
+                                    )
+                                })}
+                                <option>Sede 1</option>
+                                <option>Sede 2</option>
+                            </select>
+                        </div>
+                        <div className="col-md-2"></div>
+                    </div>
+                    
+                    <div className="row">
+                        <div className="col-md-2"></div>
+                        <div className="col-md-8">
                             <h5>Datos Personales</h5>
                             <hr></hr>
                         </div>
@@ -19,8 +109,8 @@ export default class Inscripcion extends Component{
                     <div className="form-row">
                         <div className="col-md-2"></div>
                         <div className="form-group col-md-4">
-                            <label htmlFor="nombre">Nombre</label>
-                            <input type="text" className="form-control" id="nombre" placeholder="Nombre"></input>
+                            <label htmlFor="nombre">Nombres</label>
+                            <input type="text" className="form-control" id="nombre" placeholder="Primer y segundo nombre"></input>
                         </div>
                         <div className="form-group col-md-4">
                             <label htmlFor="apellidos">Apellidos</label>
@@ -60,37 +150,9 @@ export default class Inscripcion extends Component{
                         </div>
                         <div className="col-md-2"></div>
                     </div>
-                    <div className="row">
-                        <div className="col-md-2"></div>
-                        <div className="col-md-8">
-                            <h5>Datos del Taller</h5>
-                            <hr></hr>
-                            <p>Selecciona el nivel del taller de programación (básico, medio o avanzado), y luego escoge en cuál de las sedes disponibles te quieres inscribir. </p>
-                        </div>
-                        <div className="col-md-2"></div>
-                    </div>
-                    <div className="form-row">
-                        <div className="col-md-2"></div>
-                        <div className="form-group col-md-4">
-                            <label htmlFor="nivel">Nivel</label>
-                            <select id="nivel" className="form-control">
-                                <option defaultValue>Básico</option>
-                                <option>Intermedio</option>
-                                <option>Avanzado</option>
-                            </select>
-                        </div>
-                        <div className="form-group col-md-4">
-                            <label htmlFor="sede">Sede</label>
-                            <select id="sede" className="form-control">
-                                <option defaultValue>Selecciona...</option>
-                                <option>Sede 1</option>
-                                <option>Sede 2</option>
-                            </select>
-                        </div>
-                        <div className="col-md-2"></div>
-                    </div>
+                    
                     <div align="center">
-                    <button type="submit" style={{paddingRight:"30px",marginTop:"20px"}} className={"btn text-white btn-danger"}><i className="fas icono fa-paper-plane"></i>Enviar</button>
+                        <button type="submit" style={{paddingRight:"30px",marginTop:"20px"}} className={"btn text-white btn-danger"}><i className="fas icono fa-paper-plane"></i>Enviar</button>
                     </div>
                 </form>
             </div>
